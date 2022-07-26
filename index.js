bd4b0ec3c7b9acd410c84d02211c145f8d2c3c1d
Christian Guyton <cguyton@icstars.org>
2022-07-25 23:16:46 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let j = 0;
+while (j <= 5) {
+    console.log(j);
+    j++;
+}
+
+
+let k = 0;
+while (k > 5) {
+    console.log(k);
+    k++;
+}
+
+//NOTE For loops
+// for (initializer; condition; iteration) {
+
+// }
+console.log(`---separator---`)
+
+for (let i=0; i <=5; i++) {
+    console.log(i)
+}
